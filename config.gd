719628614aaa8ddd7281609868b2ce46781113f0
Isaac Beverly <imbev@protonmail.com>
2024-05-20 17:49:51 -0500

    Add version number

--- config.gd
+++ config.gd
@@ -5,6 +5,9 @@ var config: ConfigFile
 const FILE_PATH = "user://settings.cfg"
 const KEY_SECTION = "settings"
 
+const VERSION = "0.1.0"
+const KEY_VERSION = "version"
+
 const DEFAULT_MUSIC_ENABLED = true
 const KEY_MUSIC_ENABLED = "music_enabled"
 
@@ -14,6 +17,8 @@ const KEY_SOUND_EFFECTS_ENABLED = "sound_effects_enabled"
 func _ready():
 	config = ConfigFile.new()
 	config.load(FILE_PATH)
+	
+	config.set_value(KEY_SECTION, KEY_VERSION, VERSION)
 	
 	config.set_value(KEY_SECTION, KEY_MUSIC_ENABLED, 
 		config.get_value(KEY_SECTION, KEY_MUSIC_ENABLED, 
--- menu/AboutMenu.gd
+++ menu/AboutMenu.gd
@@ -2,7 +2,9 @@ extends VBoxContainer
 
 @onready var main_menu: VBoxContainer = %MainMenu
 @onready var about_menu: VBoxContainer = %AboutMenu
-
+@onready var version_label: Label = %VersionLabel
+func _ready():
+	version_label.text = "Version " + Config.VERSION
 
 func _on_about_back_button_pressed():
 	about_menu.visible = false
